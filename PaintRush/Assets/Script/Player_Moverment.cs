c87a669c12d86fb5a76cd1012ae3c9b227a53fbe
Chloe Culver <c.culver831@gmail.com>
2019-02-02 11:12:36 -0800

    Added Paint Rollers

---
+++ PaintRush/Assets/Script/Player_Moverment.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player_Moverment : MonoBehaviour
+{
+    public float speed = 5f;
+    public float jumpSpeed = 10f;
+    public float rotationSpeed = 100f;
+    private Rigidbody rb;
+    private Vector3 moveDirection = Vector3.zero;
+    private float heading;
+    void Update()
+    {
+
+        float moveVert = Input.GetAxis("Horizontal") * speed;
+        float moveHor = Input.GetAxis("Vertical") * speed;
+
+        moveVert *= Time.deltaTime;
+        moveHor *= Time.deltaTime;
+    }
+}
