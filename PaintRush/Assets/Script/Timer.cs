fb873db56f226e41d73b99836000ceb9bafe9c51
Chloe Culver <c.culver831@gmail.com>
2019-02-02 18:18:41 -0800

    added stage timer and fied some controller issues

---
+++ PaintRush/Assets/Script/Timer.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public class Timer : MonoBehaviour
+{
+    [SerializeField] private Text uiText;
+    [SerializeField] private float mainTimer;
+
+    private float timer;
+    private bool canCount = true;
+    private bool doOnce = false;
+    private void Start()
+    {
+        timer = mainTimer;
+    }
+    private void Update()
+    {
+        if(timer >= 0.0f && canCount)
+        {
+            timer -= Time.deltaTime;
+            uiText.text = timer.ToString("F");
+        }
+        else if(timer <= 0.0f && !doOnce)
+        {
+            canCount = false;
+            doOnce = true;
+            uiText.text = "0.00";
+            timer = 0.0f;
+            SceneManager.LoadScene("Main Menu");
+        }
+    }
+
+}
