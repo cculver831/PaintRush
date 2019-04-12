21549738e190b16cb73ce3e4990990c9e9c297d0
Chloe Culver <c.culver831@gmail.com>
2019-04-12 16:53:14 -0700

    Added new powerUp,organized files, and created level select (in progress)

---
+++ PaintRush/Assets/PowerUps/PaintSwitch.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PaintSwitch : MonoBehaviour
+{
+    public static float speed;
+    void OnTriggerEnter(Collider other)
+    {
+        if (other.CompareTag("Player1"))
+        {
+            TheOlSwticheroo(other);
+        }
+        else if (other.CompareTag("Player 2"))
+        {
+            TheOlSwitcheroo2(other);
+        }
+    }
+    void TheOlSwticheroo(Collider player)
+    {
+        splatPaint switcher = GameObject.Find("Player2").GetComponent<splatPaint>();
+        switcher.change(0);
+        Destroy(gameObject);
+    }
+    void TheOlSwitcheroo2(Collider player)
+    {
+        splatPaint switcher= GameObject.Find("Player1").GetComponent<splatPaint>();
+        switcher.change(3);
+        Destroy(gameObject);
+    }
+}
--- PaintRush/Assets/splatPaint.cs
+++ PaintRush/Assets/splatPaint.cs
@@ -83,4 +83,8 @@ public class splatPaint : MonoBehaviour
     {
         splatScale = 7;
     }
+    public void change(int index)
+    {
+        colorIndex = index;
+    }
 }
