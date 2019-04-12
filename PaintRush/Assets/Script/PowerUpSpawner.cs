7fba348d129dc047998f1411d268522839381280
nguyenhanson1 <nguyenhanson1@gmail.com>
2019-04-12 15:05:15 -0700

    powerSpawner

---
+++ PaintRush/Assets/Script/PowerUpSpawner.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PowerUpSpawner : MonoBehaviour
+{
+    public GameObject[] powerUps;
+    public Transform[] spawnLocations;
+    private float timeSpawns;
+    public float timeBTWs;
+    // Use this for initialization
+    void Start()
+    {
+        timeSpawns = timeWeaBTWs;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        //weapons
+        if (timeSpawns <= 0)
+        {
+            int randWeaInd = Random.Range(0, powerUps.Length);
+            int randWeaLocInd = Random.Range(0, spawnLocations.Length);
+            Instantiate(powerUps[randWeaInd], spawnLocations[randWeaLocInd].position, Quaternion.identity);
+            timeSpawns = timeBTWs;
+        }
+        else
+        {
+            timeSpawns -= Time.deltaTime;
+        }
+    }
+}
